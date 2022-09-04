ed49c187d24749e5500c2cd5e5d396ff2d6ed989
Michal Ladryk <ladryk.michal@gmail.com>
2022-09-04 14:10:58 +0200

    App module and main file configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { AppConfigModule } from './config/app/app-config.module';
+import { MailConfigModule } from './config/mail/mail-config.module';
+import { MailProviderModule } from './providers/mail-provider.module';
+import { SendMailModule } from './mail/send/send-mail.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AppConfigModule,
+    MailConfigModule,
+    MailProviderModule,
+    SendMailModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { AppConfigService } from './config/app/app-config.service';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as defaultCluster from 'cluster';
+import { cpus } from 'os';
+
+const cluster: any = defaultCluster;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const numCPUs = cpus().length;
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const appConfig: AppConfigService = app.get(AppConfigService);
+
+  // if (cluster.isMaster) {
+  //   console.log(`Master server started on ${process.pid}`);
+  //   for (let i = 0; i < numCPUs; i++) {
+  //     cluster.fork();
+  //   }
+  //   cluster.on('exit', (worker, code, signal) => {
+  //     console.log(`Worker ${worker.process.pid} died. Restarting`);
+  //     cluster.fork();
+  //   });
+  // } else {
+  //   console.log(`Cluster server started on ${process.pid}`);
+
+  // app.enableCors({
+  //   origin: 'http://localhost:4001',
+  // });
+  await app.listen(appConfig.port);
+  // }
 }
 bootstrap();
