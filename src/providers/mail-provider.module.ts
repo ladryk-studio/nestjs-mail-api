eb54bc2ddcaf3de499eda113efa90b8fb683316c
Michal Ladryk <ladryk.michal@gmail.com>
2022-09-04 14:10:43 +0200

    Providers

---
+++ src/providers/mail-provider.module.ts
@@ -0,0 +1,36 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { Module } from '@nestjs/common';
+import { join } from 'path';
+import { MailConfigModule } from 'src/config/mail/mail-config.module';
+import { MailConfigService } from 'src/config/mail/mail-config.service';
+
+@Module({
+  imports: [
+    MailerModule.forRootAsync({
+      imports: [MailConfigModule],
+      useFactory: async (mailConfigService: MailConfigService) => ({
+        transport: {
+          host: mailConfigService.host,
+          secure: false,
+          auth: {
+            user: mailConfigService.username,
+            pass: mailConfigService.password,
+          },
+        },
+        defaults: {
+          from: `"Ladryk - no reply" <${mailConfigService.username}>`,
+        },
+        template: {
+          dir: join(__dirname, '..', 'mail/templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+      inject: [MailConfigService],
+    }),
+  ],
+})
+export class MailProviderModule {}
